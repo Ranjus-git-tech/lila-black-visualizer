3f5ddfaec753148c3315c07623cfcdcf43c51872
Ranju <ranjupodder74@gmail.com>
2026-04-07 07:15:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -212,7 +212,7 @@ if view_mode == "Movement":
         "Loot": "Loot"
     }
     for evt, style in event_styles.items():
-    sub = df[df["event"] == evt]
+        sub = df[df["event"] == evt]
 
     # 🔥 ONLY split combat events
     if evt in ["Kill", "BotKill", "Killed", "BotKilled"]:
